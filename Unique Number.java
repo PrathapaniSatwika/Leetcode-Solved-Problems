ae4f498787c7c5cd217376dd944cf426154543fe
Prathapani Satwika <107547764+PrathapaniSatwika@users.noreply.github.com>
2023-04-16 19:39:43 +0530

    Add files via upload

---
+++ Unique Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(!map.containsKey(arr[i])){
+                map.put(arr[i],1);
+            }
+            else{
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+        }
+        Set<Integer> uniqueNumber=new HashSet<Integer>(map.values());
+        return map.size()==uniqueNumber.size();
+    }
+}
